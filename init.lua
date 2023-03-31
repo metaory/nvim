caa8c35d057ff6f1e19ffa4445628de885e448b3
Pou Yan <metaory@gmail.com>
2023-03-31 11:05:52 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@
 require("config.lazy")
 require("config.setups")
 
+local XC = require("mxc.palette")
+vim.cmd('hi! gitblame guibg=#292e42' .. ' guifg=' .. XC.xfg)
+
 vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
 vim.cmd([[hi SignColumn guibg=NONE ctermbg=NONE]])
 -- vim.cmd([[hi CursorLineSign guibg=NONE ctermbg=NONE]])
