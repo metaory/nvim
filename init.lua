a1403869f9aef06e00789fb27ee4f73e3df0a747
metaory <metaory@gmail.com>
2023-04-06 16:17:10 +0800

    gitblame

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ require("config.lazy")
 require("config.setups")
 
 local XC = require("mxc.palette")
-vim.cmd('hi! gitblame guibg=#292e42' .. ' guifg=' .. XC.xfg)
 
 vim.cmd([[hi Normal            guibg=NONE ctermbg=NONE]])
 vim.cmd([[hi NormalNC          guibg=NONE ctermbg=NONE]])
@@ -14,6 +13,7 @@ vim.cmd([[hi NeoTreeSignColumn guibg=NONE ctermbg=NONE]])
 vim.cmd('hi NotifyBackground   guibg=' .. XC.xbg ..' ctermbg=NONE')
 vim.cmd('hi CursorLine         guibg=' .. XC.sk1 ..' ctermbg=NONE')
 vim.cmd('hi CursorColumn       guibg=' .. XC.sk1 ..' ctermbg=NONE')
+vim.cmd('hi gitblame           guibg=' .. XC.sk1 ..' guifg=' .. XC.xfg)
 
 -- vim.cmd([[hi CursorLine   guibg=XC.sk2]])
 -- vim.cmd([[hi CursorColumn guibg=XC.sk2]])
